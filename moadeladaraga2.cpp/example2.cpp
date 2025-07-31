37eca1a7a6af31c4aa3f57e150400135c963aa8c
shekofaamiri <shekofaamiri1@gmail.com>
2025-07-31 18:25:49 +0330

    if condition code

--- moadeladaraga2.cpp/example2.cpp
+++ moadeladaraga2.cpp/example2.cpp
@@ -8,3 +8,12 @@ int main()
     cout << "Enter a,b,c";
     cin >> a >> b >> c;
     if (a!=0)
+    {
+        d = b * b - 4 * a * c;
+        if (d > 0)
+        {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            x2 = (-b - sqrt(d)) / (2 * a);
+            cout << "x1=" << x1 << endl;
+            cout << "x2=" << x2;
+        }
