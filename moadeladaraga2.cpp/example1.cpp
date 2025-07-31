995d9df45fd20abed63069ae136f702b162cac18
shekofaamiri <shekofaamiri1@gmail.com>
2025-07-31 18:02:35 +0330

    variable code is added

---
+++ moadeladaraga2.cpp/example1.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a, b, c, d,x, x1, x2;
+    cout << "Enter a,b,c";
+    cin >> a >> b >> c;
+    if (a!=0)
+    {
+        d = b * b - 4 * a * c;
+        if (d > 0)
+        {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            x2 = (-b - sqrt(d)) / (2 * a);
+            cout << "x1=" << x1 << endl;
+            cout << "x2=" << x2;
+        }
+       else if (d == 0)
+        {
+            x = -b / (2 * a);
+            cout << "یک ریشه حقیقی (دو ریشه برابر): " << x << endl;
+        }
+        else
+        
+            cout << "معادله ریشه‌ی حقیقی ندارد (ریشه‌ها موهومی‌اند)." << endl;
+        
+    
+    }
+}
+
---
+++ moadeladaraga2.cpp/example2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a, b, c, d,x, x1, x2;
+    cout << "Enter a,b,c";
+    cin >> a >> b >> c;
+    if (a!=0)
